5e6bc29388095b1f828510d8f558fcc83e404fe8
Shoyebaktar Shirol <shoyebaktarshirol@gmail.com>
2024-07-15 17:27:17 +0530

    Updated

--- user.php
+++ user.php
@@ -1,5 +1,12 @@
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form</title>
+</head>
+<body>
+  
 
 <?php
 
@@ -73,4 +80,6 @@ $sql = "INSERT INTO `crud` (`name`, `email`, `mobile`, `password`)
 
 
   </body>
+</html>
+</body>
 </html>
